dfb7506a5e72efb30ff95924c9d29a7d6f12fe9b
PindoliaRajshree <pindoliaraji@gmail.com>
2023-04-17 11:53:59 +0530

    Added Insert Program Form with Validation

---
+++ lib/add_program_form.dart
@@ -0,0 +1,178 @@
+import 'package:fitness_app/models/program_model.dart';
+import 'package:flutter/material.dart';
+
+class AddProgramForm extends StatefulWidget {
+  final TextEditingController programNameController;
+  final bool? isEdit;
+  final List<ProgramModel> list;
+
+  const AddProgramForm(
+      {Key? key,
+      required this.programNameController,
+      this.isEdit,
+      required this.list})
+      : super(key: key);
+
+  @override
+  State<AddProgramForm> createState() => _AddProgramFormState();
+}
+
+class _AddProgramFormState extends State<AddProgramForm> {
+  var formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Center(
+        child: Container(
+          margin: EdgeInsets.only(left: 20, right: 20),
+          width: MediaQuery.of(context).size.width,
+          color: Colors.white,
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(
+                  height: 10,
+                ),
+                Stack(
+                  children: [
+                    Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        widget.isEdit!
+                            ? 'Edit Fitness Programs'
+                            : 'Add Fitness Programs',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.teal,
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.of(context).pop(false);
+                        },
+                        child: Padding(
+                          padding: EdgeInsets.only(right: 10),
+                          child: Icon(
+                            Icons.close,
+                            size: 25,
+                            color: Colors.teal,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Expanded(
+                      child: TextFormField(
+                        controller: widget.programNameController,
+                        cursorColor: Colors.black,
+                        validator: (String? value) {
+                          bool result = false;
+                          widget.list.forEach((element) {
+                            if (element.name == value) result = true;
+                          });
+                          if (value!.isEmpty) {
+                            return 'Please Enter Program Name';
+                          } else if (result) {
+                            return 'Program Name Already Exist';
+                          }
+                          return null;
+                        },
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w500,
+                        ),
+                        decoration: InputDecoration(
+                          isDense: true,
+                          hintText: "Program Name",
+                          hintStyle: TextStyle(
+                            color: Colors.black54,
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.black54,
+                              width: 2.0,
+                            ),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.black,
+                              width: 2.0,
+                            ),
+                          ),
+                        ),
+                        textInputAction: TextInputAction.next,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  children: [
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (formKey.currentState!.validate()) {
+                            Navigator.of(context).pop(true);
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Text(
+                            widget.isEdit! ? "Save" : "Add",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.normal,
+                              fontSize: 20.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
